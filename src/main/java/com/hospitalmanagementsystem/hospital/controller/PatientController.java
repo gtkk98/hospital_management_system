fb8c7e98fab5e36307f4cafa942c8742d075be03
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-04-15 19:59:45 +0530

    Patient controller -> HTTP method added.

--- src/main/java/com/hospitalmanagementsystem/hospital/controller/PatientController.java
+++ src/main/java/com/hospitalmanagementsystem/hospital/controller/PatientController.java
@@ -1,13 +1,13 @@
 package com.hospitalmanagementsystem.hospital.controller;
 
+import com.hospitalmanagementsystem.hospital.dto.request.PatientRequest;
 import com.hospitalmanagementsystem.hospital.dto.response.PatientResponse;
 import com.hospitalmanagementsystem.hospital.service.PatientService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +28,28 @@ public class PatientController {
     public ResponseEntity<List<PatientResponse>> searchPatients(
             @RequestParam(defaultValue = "") String name) {
         return ResponseEntity.ok(patientService.searchPatient(name));
+    }
+
+    // POST /api/patients
+    @PostMapping
+    public ResponseEntity<PatientResponse> createPatient(
+            @Valid @RequestBody PatientRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(patientService.createPatient(request));
+    }
+
+    // PUT /api/patients/{id}
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientResponse> updatePatient(
+            @PathVariable Long id,
+            @Valid @RequestBody PatientRequest request) {
+        return ResponseEntity.ok(patientService.updatePatient(id, request));
+    }
+
+    // DELETE /api/patinets/{id}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable Long id) {
+        patientService.deletePatient(id);
+        return ResponseEntity.noContent().build();
     }
 }
