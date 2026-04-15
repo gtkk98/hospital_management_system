63901522ce365c42c6d672b35f43c86d4013d289
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-04-15 20:01:50 +0530

    PatientNotFoundException added for handle error.

---
+++ src/main/java/com/hospitalmanagementsystem/hospital/exception/PatientNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hospitalmanagementsystem.hospital.exception;
+
+public class PatientNotFoundException extends RuntimeException {
+    public PatientNotFoundException(Long id) {
+        super("Patient not found with id: " + id);
+    }
+}
