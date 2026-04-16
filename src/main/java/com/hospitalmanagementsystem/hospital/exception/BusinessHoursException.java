20a5a3a101da46a3a0b8e98e789712ec3a1f9938
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-04-16 15:18:44 +0530

    Create exception to handle BusinessHoursException errors.

---
+++ src/main/java/com/hospitalmanagementsystem/hospital/exception/BusinessHoursException.java
@@ -0,0 +1,7 @@
+package com.hospitalmanagementsystem.hospital.exception;
+
+public class BusinessHoursException extends RuntimeException {
+    public BusinessHoursException(String message) {
+        super(message);
+    }
+}
