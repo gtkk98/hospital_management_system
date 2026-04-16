4c96e4e667c51f9f0799453474740798319c99b7
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-04-16 15:19:08 +0530

    Create exception to handle AppointmentNotFoundException errors.

---
+++ src/main/java/com/hospitalmanagementsystem/hospital/exception/AppointmentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hospitalmanagementsystem.hospital.exception;
+
+public class AppointmentNotFoundException extends RuntimeException {
+    public AppointmentNotFoundException(Long id) {
+        super("Appointment not found with id: " + id);
+    }
+}
