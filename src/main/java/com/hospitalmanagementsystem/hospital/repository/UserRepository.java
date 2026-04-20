f2d297cca0d0859339606f409e7c9922b31d08f0
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-04-20 15:16:43 +0530

    UserRepository done.

---
+++ src/main/java/com/hospitalmanagementsystem/hospital/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.hospitalmanagementsystem.hospital.repository;
+
+import com.hospitalmanagementsystem.hospital.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
