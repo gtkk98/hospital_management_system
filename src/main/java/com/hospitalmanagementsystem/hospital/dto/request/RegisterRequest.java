0276c0a92a65cf6d4863ff5ba30ced2e377dab49
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-04-20 15:17:45 +0530

    RegisterRequest done.

---
+++ src/main/java/com/hospitalmanagementsystem/hospital/dto/request/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.hospitalmanagementsystem.hospital.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class RegisterRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+    @NotBlank
+    @Pattern(regexp = "ADMIN|DOCTOR|PATIENT", message = "Role must be either ADMIN, DOCTOR, or PATIENT")
+    private String role;
+}
